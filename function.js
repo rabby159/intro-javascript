4090e7ac082c7f92ec7a59572df04f60c77215f7
Md Rabby <ablzerabbi159@gmail.com>
2023-08-04 18:33:20 +0600

    return

--- function.js
+++ function.js
@@ -1,8 +1,20 @@
-function add(num1, num2){
-    var sum = num1 + num2;
+// function add(num1, num2){
+//     var sum = num1 + num2;
     
-    return sum;
+//     return sum;
+// }
+
+// var total = add(121, 24)
+// console.log("Total:", total);
+
+function singaraKhabo(money){
+
+    var singaraPrice = 10;
+    var quantity = money / singaraPrice;
+     
+    return quantity;
 }
 
-var total = add(21, 24)
-console.log("Total:", total);
+var totalTk = 130;
+var singaras = singaraKhabo(totalTk);
+console.log("Eating singaras: ", singaras);
